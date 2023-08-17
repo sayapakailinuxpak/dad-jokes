ced825364980f7a5b0c40ef54dae973a976b341f
sayapakailinuxpak <sayapakailinuxpak@gmail.com>
2023-08-17 16:09:03 +0700

    fix: dark mode, about section

--- app/src/main/java/com/eldisprojects/dadjokes/presentation/component/BottomSheet.kt
+++ app/src/main/java/com/eldisprojects/dadjokes/presentation/component/BottomSheet.kt
@@ -1,5 +1,7 @@
 package com.eldisprojects.dadjokes.presentation.components
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,18 +15,23 @@ import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.eldisprojects.dadjokes.BuildConfig
 import com.eldisprojects.dadjokes.R
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun BottomSheet(sheetState: ModalBottomSheetState) {
+fun BottomSheet(sheetState: ModalBottomSheetState, onClick: () -> Unit = {}) {
+    val context = LocalContext.current
+    val myGithub = context.getString(R.string.about_description).contains("sdjejjsd", ignoreCase = true)
     ModalBottomSheetLayout(
         sheetContent = {
             Column(
@@ -44,8 +51,16 @@ fun BottomSheet(sheetState: ModalBottomSheetState) {
                     lineHeight = 28.sp,
                     textAlign = TextAlign.Start,
                     color = MaterialTheme.colors.onSurface,
-                    modifier = Modifier.padding(bottom = 40.dp)
+                    modifier = Modifier
+                        .padding(bottom = 40.dp)
+                        .clickable {
+                            onClick()
+                        }
                 )
+
+//                AndroidView(factory = {
+//                    Text(text = )
+//                })
             }
 
         },
